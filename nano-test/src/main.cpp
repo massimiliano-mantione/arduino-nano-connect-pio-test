afaa6274fdc059bdc5b1cfd4400a836f8c4c4e2e
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2022-08-26 12:12:18 +0200

    VL53L3CX test works.

--- nano-test/src/main.cpp
+++ nano-test/src/main.cpp
@@ -6,6 +6,25 @@
 #include <vl53lx_class.h>
 #include <arducam.h>
 
+#include <vl53lx_class.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#define DEV_I2C Wire
+#define SerialPort Serial
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 13
+#endif
+#define LedPin LED_BUILTIN
+
+// Components.
+VL53LX sensor_vl53lx_sat(&DEV_I2C, A1);
+
 #include <rust_lib.h>
 
 extern "C" void ffi_usb_serial_write(const char* buffer, size_t size) {
@@ -17,7 +36,7 @@ uint8_t image_tmp[162*162];
 uint8_t image[96*96];
 uint8_t header[2] = {0x55,0xAA};
 
-void setup() {
+void setup_with_arducam() {
   Serial.begin(115200);
 
   //gpio_init(PIN_LED);
@@ -43,10 +62,80 @@ void setup() {
 	arducam_init(&config);
 }
 
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(LedPin, OUTPUT);
+
+   SerialPort.println("Starting...");
+
+   // Initialize I2C bus.
+   DEV_I2C.begin();
+
+   // Configure VL53LX satellite component.
+   sensor_vl53lx_sat.begin();
+
+   // Switch off VL53LX satellite component.
+   sensor_vl53lx_sat.VL53LX_Off();
+
+   //Initialize VL53LX satellite component.
+   sensor_vl53lx_sat.InitSensor(0x12);
+
+   // Start Measurements
+   sensor_vl53lx_sat.VL53LX_StartMeasurement();
+}
+
 int count = 0;
 
-void loop() {
+void loop_test_ffi() {
   count++;
   print_tick(count);
   sleep_ms(1000);
+}
+
+void loop()
+{
+   VL53LX_MultiRangingData_t MultiRangingData;
+   VL53LX_MultiRangingData_t *pMultiRangingData = &MultiRangingData;
+   uint8_t NewDataReady = 0;
+   int no_of_object_found = 0, j;
+   char report[64];
+   int status;
+
+   do
+   {
+      status = sensor_vl53lx_sat.VL53LX_GetMeasurementDataReady(&NewDataReady);
+   } while (!NewDataReady);
+
+   //Led on
+   digitalWrite(LedPin, HIGH);
+
+   if((!status)&&(NewDataReady!=0))
+   {
+      status = sensor_vl53lx_sat.VL53LX_GetMultiRangingData(pMultiRangingData);
+      no_of_object_found=pMultiRangingData->NumberOfObjectsFound;
+      snprintf(report, sizeof(report), "VL53LX Satellite: Count=%d, #Objs=%1d ", pMultiRangingData->StreamCount, no_of_object_found);
+      SerialPort.print(report);
+      for(j=0;j<no_of_object_found;j++)
+      {
+         if(j!=0)SerialPort.print("\r\n                               ");
+         SerialPort.print("status=");
+         SerialPort.print(pMultiRangingData->RangeData[j].RangeStatus);
+         SerialPort.print(", D=");
+         SerialPort.print(pMultiRangingData->RangeData[j].RangeMilliMeter);
+         SerialPort.print("mm");
+         SerialPort.print(", Signal=");
+         SerialPort.print((float)pMultiRangingData->RangeData[j].SignalRateRtnMegaCps/65536.0);
+         SerialPort.print(" Mcps, Ambient=");
+         SerialPort.print((float)pMultiRangingData->RangeData[j].AmbientRateRtnMegaCps/65536.0);
+         SerialPort.print(" Mcps");
+      }
+      SerialPort.println("");
+      if (status==0)
+      {
+         status = sensor_vl53lx_sat.VL53LX_ClearInterruptAndStartMeasurement();
+      }
+   }
+
+   digitalWrite(LedPin, LOW);
 }
